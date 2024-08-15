dc79ea19a56fdbb6dd71afc46d365905f27c61aa
y1141335509 <y1141335509@gmail.com>
2024-08-14 17:54:14 -0700

    created the Story Block class

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 using UnityEngine.UI;       // 只要是在Unity游戏中使用了 UI（例如canvas，text， button等）就必须导入 UnityEngine.UI
 
 
+class StoryBlock {
+    public string story;
+    public string option1Text;
+    public string option2Text;
+    public StoryBlock option1Block;
+    public StoryBlock option2Block;
+
+    public StoryBlock(string story, string option1Text="", string option2Text="", 
+    StoryBlock option1Block = null, StoryBlock option2Block = null) {
+        this.story = story;
+        this.option1Text = option1Text;
+        this.option2Text = option2Text;
+        this.option1Block = option1Block;
+        this.option2Block = option2Block;
+    }
+}
+
 public class GameManager : MonoBehaviour
 {
 
